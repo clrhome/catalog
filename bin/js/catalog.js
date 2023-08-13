5883e90f27801eff24c229b0f5803aa5ba4bebe0
Deep Toaster <deeptoaster@gmail.com>
2023-08-13 00:43:39 -0400

    move search filtering off jQuery

--- bin/js/catalog.js
+++ bin/js/catalog.js
@@ -1,57 +1,144 @@
-function navigateGalleryFromHash() {
-  if (window.location.hash.length <= 1) {
-    $(".left a.active").removeClass("active");
-    $(".values").first().scrollTop(0);
-  } else {
-    $("[href=" + window.location.hash + "]")
-      .not(".active")
-      .mousedown();
+const ACTIVE_CLASS = "active";
+const UNMATCHED_CLASS = "unmatched";
+
+function classRegExp(className) {
+  return new RegExp("\\s*" + className + "\\b");
+}
+
+function filterLeft(left, query) {
+  let matchCount = 0;
+
+  for (
+    let leftChildIndex = 0;
+    leftChildIndex < left.childNodes.length;
+    leftChildIndex++
+  ) {
+    const leftChild = left.childNodes.item(leftChildIndex);
+
+    if (leftChild.nodeName === "A") {
+      let leftMatchCount = 0;
+
+      const right = document.getElementById(
+        leftChild.getAttribute("href").slice(1)
+      );
+
+      for (
+        let rightChildIndex = 0;
+        rightChildIndex < right.childNodes.length;
+        rightChildIndex++
+      ) {
+        const rightChild = right.childNodes.item(rightChildIndex);
+
+        if (
+          rightChild.nodeType === Node.ELEMENT_NODE &&
+          /\bleft\b/.test(rightChild.className)
+        ) {
+          leftMatchCount += filterLeft(rightChild, query);
+          break;
+        }
+      }
+
+      if (
+        leftMatchCount === 0 &&
+        leftChild.innerText.toLowerCase().indexOf(query) !== -1
+      ) {
+        leftMatchCount = 1;
+      }
+
+      const unmatched = leftMatchCount === 0;
+
+      toggleClass(leftChild, UNMATCHED_CLASS, unmatched);
+      toggleClass(right, UNMATCHED_CLASS, unmatched);
+
+      if (!unmatched) {
+        matchCount += leftMatchCount;
+      }
+    }
+  }
+
+  return matchCount;
+}
+
+function hasClass(element, className) {
+  return classRegExp(className).test(element.className);
+}
+
+function toggleClass(element, className, value) {
+  element.className = element.className.replace(classRegExp(className), "");
+
+  if (value) {
+    element.className += " " + className;
   }
 }
 
-function applyTableFilter(tableSelector, filterText) {
-  var matched = false;
+function initializeSearch() {
+  let activeEntries = [];
+  const divs = document.getElementsByTagName("div");
+  let gallery;
+  const entries = {};
+  let searchTimeout = 0;
 
-  $(tableSelector)
-    .children("a")
-    .each(function () {
-      var linkedRight = $($(this).attr("href"));
-      var nestedTable = linkedRight.children(".left");
-      nestedTable = nestedTable.length
-        ? applyTableFilter(nestedTable, filterText)
-        : $(this).text().toUpperCase().indexOf(filterText) + 1;
-      linkedRight.add(this).toggleClass("hidden", !Boolean(nestedTable));
-      matched += nestedTable;
-    });
+  function clearActiveEntries() {
+    for (let entryIndex = 0; entryIndex < activeEntries.length; entryIndex++) {
+      toggleClass(activeEntries[entryIndex], ACTIVE_CLASS, false);
+    }
 
-  return matched;
-}
+    activeEntries = [];
+  }
 
-$(function () {
-  u = 0;
+  function handleMouseDown(event) {
+    selectEntry(event.target);
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  function scrollLeft() {
+    gallery.scrollLeft = 0;
+  }
+
+  function selectEntry(entry) {
+    const href = entry.getAttribute("href");
+
+    clearActiveEntries();
+
+    for (let hrefBreak = 4; hrefBreak <= href.length; hrefBreak += 2) {
+      const partialEntry = entries[href.slice(0, hrefBreak)];
+
+      partialEntry.focus();
+      partialEntry.blur();
+      toggleClass(partialEntry, "active", true);
+      activeEntries.push(partialEntry);
+    }
+
+    window.setTimeout(scrollLeft, 0);
+    window.location.href = href;
+  }
+
+  for (let divIndex = 0; divIndex < divs.length; divIndex++) {
+    const div = divs.item(divIndex);
+
+    if (hasClass(div, "gallery")) {
+      gallery = div;
+    } else if (hasClass(div, "left")) {
+      divEntries = div.getElementsByTagName("a");
+
+      for (
+        let divEntryIndex = 0;
+        divEntryIndex < divEntries.length;
+        divEntryIndex++
+      ) {
+        const divEntry = divEntries.item(divEntryIndex);
+
+        divEntry.onmousedown = handleMouseDown;
+        entries[divEntry.getAttribute("href")] = divEntry;
+      }
+    }
+  }
 
   $(".left a")
     .mousedown(function () {
       var e = $(this).attr("href");
-      $(".values .values").scrollTop(0);
-      $(this)
-        .add("[href=" + e.slice(0, 4) + "]")
-        .focus()
-        .blur()
-        .siblings()
-        .add(".values .left a.active")
-        .removeClass("active")
-        .end()
-        .end()
-        .addClass("active");
-      window.location.href = e;
       var f = $(e);
-      $(".gallery").scrollLeft(
-        $(".gallery").scrollLeft() +
-          f.position().left -
-          parseInt($(".gallery").css("margin-left"))
-      );
-
       if (f.children("dl").length) {
         $.get(
           parseInt(e.slice(2, 4), 16) +
@@ -103,7 +190,7 @@ $(function () {
         case 74:
           if (f.length) {
             f = f.last();
-            var g = f.nextUntil(":not(.hidden)");
+            var g = f.nextUntil(":not(.unmatched)");
             f = g.length ? g.last().next() : f.next();
 
             if (f.length) f.mousedown();
@@ -113,23 +200,23 @@ $(function () {
         case 13:
         case 39:
           if (!f.length) {
-            $(".left a:not(.hidden)").first().mousedown();
+            $(".left a:not(.unmatched)").first().mousedown();
             return false;
           }
 
-          $(f.attr("href") + " .left a:not(.hidden)")
+          $(f.attr("href") + " .left a:not(.unmatched)")
             .first()
             .mousedown();
           return false;
         case 38:
         case 75:
           if (!f.length) {
-            $(".left").first().children("a:not(.hidden)").last().mousedown();
+            $(".left").first().children("a:not(.unmatched)").last().mousedown();
             return false;
           }
 
           f = f.last();
-          var g = f.prevUntil(":not(.hidden)");
+          var g = f.prevUntil(":not(.unmatched)");
           f = g.length ? g.last().prev() : f.prev();
 
           if (f.length) f.mousedown();
@@ -143,7 +230,7 @@ $(function () {
     .bind("touchstart", function () {});
 
   $("input")
-    .attr("placeholder", "type here to search")
+    .attr("placeholder", "type to search\u2026 (/)")
     .keydown(function (e) {
       e.stopPropagation();
 
@@ -153,19 +240,30 @@ $(function () {
       clearTimeout(u);
 
       u = setTimeout(function () {
-        var e = $("input").removeClass("red green").val().toUpperCase();
+        var e = $("input").removeClass("red green").val().toLowerCase();
         window.location.hash = "";
 
         if (e.length)
           $("input").addClass(
-            applyTableFilter(".gallery > div > div > .left", e)
+            filterLeft($(".gallery > div > div > .left")[0], e)
               ? "green"
               : "red"
           );
-        else $(".hidden").removeClass("hidden");
+        else $(".unmatched").removeClass("unmatched");
       }, 100);
     });
 
-  window.onhashchange = navigateGalleryFromHash;
-  navigateGalleryFromHash();
-});
+  window.onhashchange = function () {
+    if (window.location.hash.length <= 2) {
+      clearActiveEntries();
+    } else {
+      if (!hasClass(entries[window.location.hash], "active")) {
+        selectEntry(entries[window.location.hash]);
+      }
+    }
+  };
+
+  window.onhashchange(document.createEvent("HashChangeEvent"));
+}
+
+document.addEventListener("DOMContentLoaded", initializeSearch);
